a49f14733e33ee3db08a2bf0f706b3c0e7b21e02
GoshoPetrov <yankrustev@gmail.com>
2023-10-20 16:26:15 +0300

    first

---
+++ Project Hangman/Project Hangman/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Project_Hangman
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
